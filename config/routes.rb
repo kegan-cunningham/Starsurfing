ee172f2fe927b6f0530eac5166abcf22151131ab
TheSlyPig <kcunningham04@hotmail.com>
2018-01-02 12:54:13 -0500

    Add Users and Sessions models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/root'
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:new, :create, :edit, :update]
+    resource :session, only: [:create, :destroy]
+  end
+
+  root 'static_pages#root'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
