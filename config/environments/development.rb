e4cd8860aafdc2e56e4e52a16459c6a50f643b8f
Kegan <kcunningham40@gmail.com>
2018-01-06 13:40:07 -0500

    Attempt to fix paperclip issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_region => ENV['s3_region'],
+    :s3_credentials => {
+      :bucket => ENV['s3_bucket'],
+      :access_key_id => ENV['s3_access_key_id'],
+      :secret_access_key => ENV['s3_secret_access_key']
+    }
+  }
 end
