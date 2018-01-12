f450a4e99097b2d48f422fb4a8ed1a3405212765
Kegan Cunningham <kcunningham04@hotmail.com>
2018-01-12 09:07:07 -0500

    Fix user image upload

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -2,7 +2,7 @@ export const signup = (user) => {
   return $.ajax({
     url: 'api/users',
     method: 'POST',
-    data: { user },
+    data: user,
     contentType: false,
     processData: false,
   });
